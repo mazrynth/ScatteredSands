78d1dc3f5fd99afbf16efc09717e5937016cd975
mazrynth <mazrynth@gmail.com>
2016-10-08 07:06:44 -0400

    Re-Upload
    Git-Ignore now seems to be ignoring the meta / library folders properly,
    just had to reset the project using the new git-ignore settings. Might
    want to delete your current project folder and completely re-sync the
    project.

---
+++ Scattered_Sands/Assets/Taylor Stuff/Camera_Follow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Follow : MonoBehaviour {
+
+	public Transform Target;
+
+	void LateUpdate () {
+		transform.position = new Vector3 (Target.position.x, transform.position.y, Target.position.z);
+	}
+}
