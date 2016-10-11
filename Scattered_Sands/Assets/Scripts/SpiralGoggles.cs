436bb052e639b3b7655f4c20706091a995bab0fe
Desmond17 <fpagan3@knights.ucf.edu>
2016-10-11 18:49:09 -0400

    adding scripts
    goggles

---
+++ Scattered_Sands/Assets/Scripts/SpiralGoggles.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiralGoggles : MonoBehaviour {
+
+    public Light splight;
+    // Use this for initialization
+    void Start()
+    {
+        splight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (splight.enabled == false)
+            {
+                splight.enabled = true;
+            }
+            else
+            {
+                splight.enabled = false;
+            }
+        }
+
+    }
+}
---
+++ Scattered_Sands/Assets/Scripts/XRayLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XRayLight : MonoBehaviour {
+
+    public Light xray;
+	// Use this for initialization
+	void Start ()
+    {
+        xray = GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            if(xray.enabled == false)
+            {
+                xray.enabled = true;
+            }
+            else
+            {
+                xray.enabled = false;
+            }
+        }
+	
+	}
+}
