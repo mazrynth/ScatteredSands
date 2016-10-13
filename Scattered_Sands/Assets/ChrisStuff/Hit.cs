d2400e71eaa356d2c6392ff9c2854f4bdc9d633f
cdavenport123 <cdavenport@knights.ucf.edu>
2016-10-13 18:13:06 -0400

    Rock throwing and navigation

---
+++ Scattered_Sands/Assets/ChrisStuff/Hit.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hit : MonoBehaviour {
+
+	public RaycastHit pub_thing;
+	public Transform pickup;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Ray line = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Debug.DrawRay (line.origin, line.direction * 200000, Color.yellow);
+		RaycastHit thing;
+		if (Physics.Raycast (this.transform.position, line.direction, out thing, 10) && Input.GetKeyDown (KeyCode.E)) {
+			pub_thing = thing;
+			if (pub_thing.transform.tag == "Rock") {
+				pub_thing.transform.position = pickup.transform.position;
+				pub_thing.transform.SetParent (pickup);
+				pub_thing.rigidbody.useGravity = false;
+				pub_thing.rigidbody.isKinematic = true;
+				Debug.Log ("Picked");
+			}
+		}
+			if (Input.GetButtonDown ("Fire1")) {
+				NavAgent.thrown = true;
+				pub_thing.rigidbody.isKinematic = false;
+				pub_thing.rigidbody.useGravity = true;
+				pickup.DetachChildren ();
+				pub_thing.rigidbody.AddForce (Camera.main.transform.forward * 500);
+				Debug.Log ("Throw");
+			}
+		}
+	}
---
+++ Scattered_Sands/Assets/ChrisStuff/NavAgent.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavAgent : MonoBehaviour {
+
+
+	public Transform[] waypoints;
+	public float timer;
+	public GameObject Ai;
+	public GameObject player;
+	public GameObject rock;
+	public static NavMeshAgent agent;
+	public int wp = 0;
+	public static bool thrown;
+
+
+	// Use this for initialization
+	void Start () {
+		agent = Ai.GetComponent <NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//timer += Time.deltaTime;
+		if (agent.remainingDistance < 0.5) {
+			if (wp == 0) {
+				agent.SetDestination (waypoints [wp++].position);
+			} else {
+				agent.SetDestination (waypoints [wp].position);
+				wp++;
+			}
+		}
+			
+		}
+
+	void OnTriggerStay (Collider other){
+		if (wp == 4) {
+			wp = 0;
+		}
+		if (other.gameObject.CompareTag ("Player")) {
+			player = other.gameObject;
+			agent.SetDestination (player.transform.position);
+		} else if (other.gameObject.CompareTag ("Rock")) {
+			rock = other.gameObject;
+			if (timer < 3 && thrown == true) {
+				timer += Time.deltaTime;
+				agent.SetDestination (rock.transform.position);
+			} else {
+				agent.SetDestination (waypoints [wp].position);
+				thrown = false;
+			}
+		} 
+	}
+}
+
