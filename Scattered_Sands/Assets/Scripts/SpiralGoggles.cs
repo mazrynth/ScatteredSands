5890cbeab379a0296428726c1cae054e6bc4e841
Desmond17 <fpagan3@knights.ucf.edu>
2016-10-14 00:54:03 -0400

    Working on cooldown

--- Scattered_Sands/Assets/Scripts/SpiralGoggles.cs
+++ Scattered_Sands/Assets/Scripts/SpiralGoggles.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class SpiralGoggles : MonoBehaviour {
 
     public Light splight;
+    public float coolDown = 20;
+    public float coolDownTimer;
+
     // Use this for initialization
     void Start()
     {
@@ -13,17 +16,41 @@ public class SpiralGoggles : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+           //if (splight.enabled == false)
+            //{
+                //splight.enabled = true;
+            //}
+            //else
+            //{
+                //splight.enabled = false;
+            //}
+        //}
+
+        if(coolDownTimer > 0)
         {
-            if (splight.enabled == false)
-            {
-                splight.enabled = true;
-            }
-            else
-            {
-                splight.enabled = false;
-            }
+            coolDownTimer -= Time.deltaTime;
         }
 
+        if(coolDownTimer < 0)
+        {
+            coolDownTimer = 0;
+        }
+
+        if(Input.GetKeyDown(KeyCode.R) && coolDownTimer == 0)
+        {
+            enableLight();
+            coolDownTimer = coolDown;
+        }
+
+    }
+
+    void enableLight()
+    {
+        if(splight.enabled == false)
+        {
+            splight.enabled = true;
+        }
     }
 }
