f8436733207f49102bfc431a8d28e21f92603b27
Desmond17 <fpagan3@knights.ucf.edu>
2016-10-18 19:05:20 -0400

    added hitboxes for goggles

--- Scattered_Sands/Assets/Scripts/SpiralGoggles.cs
+++ Scattered_Sands/Assets/Scripts/SpiralGoggles.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class SpiralGoggles : MonoBehaviour {
 
     public Light splight;
-    public float coolDown;
-    public float coolDownTimer;
-    public float timeLimit;
+    GameObject dazed;
+    public float stun;
+    public bool stunned = false;
+    //public float coolDown;
+    //public float coolDownTimer;
+    //public float timeLimit;
     // Use this for initialization
     void Start()
     {
@@ -16,67 +19,74 @@ public class SpiralGoggles : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+           if (splight.enabled == false)
+            {
+                splight.enabled = true;
+            }
+            else
+            {
+                splight.enabled = false;
+            }
+        }
+
+
+        //if(coolDownTimer > 0)
         //{
-           //if (splight.enabled == false)
-            //{
-                //splight.enabled = true;
-            //}
-            //else
-            //{
-                //splight.enabled = false;
-            //}
+            //coolDownTimer -= Time.deltaTime;
         //}
 
-        if(coolDownTimer > 0)
-        {
-            coolDownTimer -= Time.deltaTime;
-        }
+        //if(coolDownTimer < 0)
+        //{
+            //coolDownTimer = 0;
+        //}
 
-        if(coolDownTimer < 0)
-        {
-            coolDownTimer = 0;
-        }
+        //if (timeLimit > 0) 
+        //{
+            //timeLimit -= Time.deltaTime;
 
-        if(timeLimit > 0)
-        {
-            timeLimit -= Time.deltaTime;
-        }
+        //}
 
-        if(timeLimit < 0)
-        {
-            timeLimit = 0;
-        }
+        //if (timeLimit < 0)
+        //{
+            //timeLimit = 0;
+        //}
 
-        if(Input.GetKey(KeyCode.R) && coolDownTimer == 0)
-        {
-            enableLight();
+        //if (Input.GetKeyDown(KeyCode.R) && coolDownTimer == 0)
+        //{
+           
+            //enableLight();
+
+            //if (timeLimit == 0)
+            //{
+                //CoolDown();
+                //timeLimit = 20;
+            //}
+
             
-            if(timeLimit == 0)
-            {
-                CoolDown();
-            }
-
-            timeLimit = 20;
-        }
+        //}
 
     }
 
-    void enableLight()
-    {
-        if(splight.enabled == false)
-        {
-            splight.enabled = true;
-        }
-        else
-        {
-            splight.enabled = false;
-        }
-    }
+    //void enableLight()
+    //{
+        //if (splight.enabled == false)
+        //{
+            //splight.enabled = true;
+        //}
+        //else
+        //{
+            //splight.enabled = false;
+        //}
+    //}
 
-    void CoolDown()
-    {
-        splight.enabled = false;
-        coolDownTimer = coolDown;
-    }
+    //void CoolDown()
+    //{
+        //if(splight.enabled == true)
+        //{
+            //splight.enabled = false;
+        //}
+        //coolDownTimer = coolDown;
+    //}
 }
