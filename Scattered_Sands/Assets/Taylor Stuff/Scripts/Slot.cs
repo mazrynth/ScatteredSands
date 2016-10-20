ac68df1369dcd6250f7cb9886f459c217bee77c7
tywicked <tyjohn@live.com>
2016-10-20 02:03:18 -0400

    Inventory System

---
+++ Scattered_Sands/Assets/Taylor Stuff/Scripts/Slot.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour {
+
+	private Stack<Item> items;
+
+	public Text stackTxt;
+
+	public Sprite slotEmpty;
+	public Sprite slotHighlight;
+
+	public bool IsEmpty
+	{
+		get { return items.Count == 0; }
+	}
+
+	public bool IsAvailable
+	{
+		get { return CurrentItem.maxSize > items.Count; }
+	}
+
+	public Item CurrentItem
+	{
+		get {return items.Peek();}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+		items = new Stack<Item> ();
+		RectTransform slotRect = GetComponent<RectTransform>();
+		RectTransform txtRect = stackTxt.GetComponent<RectTransform>();
+
+		int txtScleFactor = (int)(slotRect.sizeDelta.x * 0.50);
+		stackTxt.resizeTextMaxSize = txtScleFactor;
+		stackTxt.resizeTextMinSize = txtScleFactor;
+
+		txtRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotRect.sizeDelta.x);
+		txtRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotRect.sizeDelta.y);
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddItem(Item item)
+	{
+		items.Push(item);
+
+		if (items.Count > 1) {
+			stackTxt.text = items.Count.ToString ();
+		} 
+
+
+		ChangeSprite(item.spriteNeutral, item.spriteHighlighted);
+		Debug.Log ("Changed2");
+	}
+
+
+	private void ChangeSprite(Sprite neutral, Sprite highlight)
+	{
+		GetComponent<Image> ().sprite = neutral;
+		SpriteState st = new SpriteState ();
+		st.highlightedSprite = highlight;
+		st.pressedSprite = neutral;
+
+		GetComponent<Button>().spriteState = st;
+		Debug.Log("Changed");
+	}
+}
