1e1b98003f3337751324a9087407bbea642956df
Desmond17 <fpagan3@knights.ucf.edu>
2016-10-20 04:35:33 -0400

    Transferred Goggles to Level 1

--- Scattered_Sands/Assets/Scripts/SpiralGoggles.cs
+++ Scattered_Sands/Assets/Scripts/SpiralGoggles.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class SpiralGoggles : MonoBehaviour {
 
     public Light splight;
-    GameObject dazed;
-    public float stun;
+    public GameObject dazed;
+    public NavMeshAgent nav;
+    public float stun = 0;
     public bool stunned = false;
+
     //public float coolDown;
     //public float coolDownTimer;
     //public float timeLimit;
@@ -14,6 +16,9 @@ public class SpiralGoggles : MonoBehaviour {
     void Start()
     {
         splight = GetComponent<Light>();
+        nav = GetComponent<NavMeshAgent>();
+        dazed = GameObject.FindWithTag("Stunned");
+        dazed.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,62 +36,101 @@ public class SpiralGoggles : MonoBehaviour {
             }
         }
 
+        if (stunned == true)
+        {
+            //start the timer for the stun 
+            stun += Time.deltaTime;
+            //stun for 3 seconds (greater than 3 seconds resumes movement)
+            if (stun < 3f)
+            {
+                //turn on the stun game object with the collider
+                dazed.gameObject.SetActive(true);
+                //stop the navigation of the object
+                nav.Stop();
 
-        //if(coolDownTimer > 0)
-        //{
-            //coolDownTimer -= Time.deltaTime;
-        //}
+            }
+            else
+            {
+                //turn off the stun game object so it doesn't hit anything else
+                dazed.gameObject.SetActive(false);
+                //set stun back to false
+                stunned = false;
+                //reset the stun timer
+                stun = 0;
+                //continue navigating
+                nav.Resume();
 
-        //if(coolDownTimer < 0)
-        //{
-            //coolDownTimer = 0;
-        //}
+            }
 
-        //if (timeLimit > 0) 
-        //{
-            //timeLimit -= Time.deltaTime;
-
-        //}
-
-        //if (timeLimit < 0)
-        //{
-            //timeLimit = 0;
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.R) && coolDownTimer == 0)
-        //{
-           
-            //enableLight();
-
-            //if (timeLimit == 0)
-            //{
-                //CoolDown();
-                //timeLimit = 20;
-            //}
-
-            
-        //}
-
+        }
     }
 
-    //void enableLight()
+    void OnTriggerEnter(Collider other)
+    {
+        //if something with the tag Stun collides with it then stunned = true;
+        if (other.gameObject.CompareTag("Stunned"))
+        {
+            Debug.Log("Stunned");
+            stunned = true;
+        }
+    }
+
+    //if(coolDownTimer > 0)
     //{
+    //coolDownTimer -= Time.deltaTime;
+    //}
+
+    //if(coolDownTimer < 0)
+    //{
+    //coolDownTimer = 0;
+    //}
+
+    //if (timeLimit > 0) 
+    //{
+    //timeLimit -= Time.deltaTime;
+
+    //}
+
+    //if (timeLimit < 0)
+    //{
+    //timeLimit = 0;
+    //}
+
+    //if (Input.GetKeyDown(KeyCode.R) && coolDownTimer == 0)
+    //{
+
+    //enableLight();
+
+    //if (timeLimit == 0)
+    //{
+    //CoolDown();
+    //timeLimit = 20;
+    //}
+
+
+    //}
+
+}
+
+
+        //void enableLight()
+        //{
         //if (splight.enabled == false)
         //{
-            //splight.enabled = true;
+        //splight.enabled = true;
         //}
         //else
         //{
-            //splight.enabled = false;
+        //splight.enabled = false;
         //}
-    //}
+        //}
 
-    //void CoolDown()
-    //{
+        //void CoolDown()
+        //{
         //if(splight.enabled == true)
         //{
-            //splight.enabled = false;
+        //splight.enabled = false;
         //}
         //coolDownTimer = coolDown;
-    //}
-}
+        //}
+    
