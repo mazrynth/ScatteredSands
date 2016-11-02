ff323848713a60cc7e0c4ec73282557bdf91e811
mazrynth <mazrynth@gmail.com>
2016-11-02 01:51:28 -0400

    Tinkering around with Mecanim animation, and hit box.
    Updated Mummy so he plays walk animation, and added a cube hit box in
    front of him to replace hit detection based on distance, should help
    with Player getting hit by the enemy through walls/ around corners.

---
+++ Scattered_Sands/Assets/Ian_Stuff/Anim_Test.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anim_Test : MonoBehaviour {
+
+	//Get References:
+	Animator anim;		//Reference to the animator controller.
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Establish reference:
+		anim = GetComponent<Animator>();
+		//anim.SetBool("Idle",false);
+		//anim.SetBool("Walk",true);
+		anim.SetTrigger("Attack");
+		//anim.SetBool("Idle",true);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+
+
+
+	}
+
+
+
+
+
+}
