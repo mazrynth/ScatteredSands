457c35da5d55d0595f18c2b9bd744f7f0022ce9c
Desmond17 <fpagan3@knights.ucf.edu>
2016-11-08 11:57:13 -0500

    Blast Couplings 50%
    1/2 of the code is complete. Also created a folder for my stuff.

---
+++ Scattered_Sands/Assets/Frank's Stuff/BlastCouplings.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlastCouplings : MonoBehaviour {
+
+    public GameObject blastCouplings;
+    GameObject clone;
+    Vector3 dir = new Vector3(0,0,3);
+    float cooldownTimer = 0;
+    float fireDelay = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cooldownTimer -= Time.deltaTime;
+
+	    if(Input.GetKeyDown(KeyCode.F) && cooldownTimer <= 0)
+        {
+            cooldownTimer = fireDelay;
+            clone = Instantiate(blastCouplings, transform.position, transform.rotation) as GameObject;
+            clone.GetComponent<Rigidbody>().AddForce(dir * 500);
+            Debug.Log("Blast Coupling shot");
+        }
+	}
+}
