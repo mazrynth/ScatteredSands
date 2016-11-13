384910b99a16ddb5895e74cde4626f62833de10f
Desmond17 <fpagan3@knights.ucf.edu>
2016-11-13 00:20:47 -0500

    Blast Couplings code works

--- Scattered_Sands/Assets/Frank's Stuff/BlastCouplings.cs
+++ Scattered_Sands/Assets/Frank's Stuff/BlastCouplings.cs
@@ -4,28 +4,28 @@ using System.Collections;
 public class BlastCouplings : MonoBehaviour {
 
     public GameObject blastCouplings;
-    GameObject clone;
-    Vector3 dir = new Vector3(0,0,3);
+    //GameObject clone;
+    Vector3 dir = new Vector3(0, 0, 5);
     float cooldownTimer = 0;
-    float fireDelay = 10.0f;
+    float fireDelay = 20.0f;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         cooldownTimer -= Time.deltaTime;
 
-	    if(Input.GetKeyDown(KeyCode.F) && cooldownTimer <= 0)
+        if (Input.GetKeyDown(KeyCode.F) && cooldownTimer <= 0)
         {
-            cooldownTimer = fireDelay;
-            clone = Instantiate(blastCouplings, transform.position, transform.rotation) as GameObject;
-            clone.GetComponent<Rigidbody>().AddForce(dir * 500);
+            GameObject clone = Instantiate(blastCouplings, transform.position, Quaternion.identity) as GameObject;
+            clone.GetComponent<Rigidbody>().AddForce(dir * 700);
             Debug.Log("Blast Coupling shot");
+            cooldownTimer = fireDelay;
         }
-	}
+    }
 }
---
+++ Scattered_Sands/Assets/Frank's Stuff/ProjectileExplosion.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileExplosion : MonoBehaviour {
+
+    float radius = 5.0f;
+    float power = 10.0f;
+    float explosiveLift = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 Origin = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(Origin, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, Origin, radius, 3.0f);
+                Destroy(gameObject, 3);
+            }
+        }
+    }
+}
